ab8da0183e5285e60c5464f7fa906613b9d0f207
Malinducj <ameshmalindu006@gmail.com>
2020-09-15 16:46:52 +0530

    Updates

--- src/dao/custom/Impl/PaymentsDAOImpl.java
+++ src/dao/custom/Impl/PaymentsDAOImpl.java
@@ -1,33 +1,69 @@
 package dao.custom.Impl;
 
+import dao.CrudUtil;
 import dao.custom.PaymentsDAO;
 import entity.Payments;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class PaymentsDAOImpl implements PaymentsDAO {
     @Override
-    public boolean add(Payments payments) {
-        return false;
+    public boolean add(Payments a) throws SQLException, ClassNotFoundException {
+        String sql ="Insert Into payments Values(?,?,?,?,?)";
+        return CrudUtil.executeUpdate(sql,a.getPayID(),a.getCustomerId(),a.getPayType(),a.getPayAmount(),a.getPayStatus());
+
     }
 
     @Override
-    public boolean delete(String s) {
-        return false;
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        String  sql ="Delete from payments where payId";
+        return CrudUtil.executeUpdate(sql,s);
     }
 
     @Override
-    public boolean update(Payments payments) {
-        return false;
+    public boolean update(Payments a) throws SQLException, ClassNotFoundException {
+
+        String sql ="Update payments set cusId =?,payType = ?,payAmount = ?,payStatus = ? where payId =?";
+        return CrudUtil.executeUpdate(sql,a.getCustomerId(),a.getPayType(),a.getPayAmount(),a.getPayStatus(),a.getPayID());
+
     }
 
     @Override
-    public Payments search(String s) {
+    public Payments search(String s) throws SQLException, ClassNotFoundException {
+
+        String sql = "Select * from payments where payId = ?";
+        ResultSet rst = CrudUtil.executeQuery(sql, s);
+        if (rst.next()){
+            return new Payments(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+                    rst.getString(5)
+
+            );
+        }
         return null;
     }
 
     @Override
-    public ArrayList<Payments> getAll() {
-        return null;
+    public ArrayList<Payments> getAll() throws SQLException, ClassNotFoundException {
+
+        String sql ="Select * from payments";
+        ResultSet rst = CrudUtil.executeQuery(sql);
+        ArrayList<Payments>list = new ArrayList<>();
+
+        while (rst.next()){
+            list.add(new Payments(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+                    rst.getString(5)
+            ));
+        }
+        return list;
     }
 }
--- src/dao/custom/Impl/ScheduleDAOImpl.java
+++ src/dao/custom/Impl/ScheduleDAOImpl.java
@@ -1,33 +1,66 @@
 package dao.custom.Impl;
 
+import dao.CrudUtil;
 import dao.custom.ScheduleDAO;
 import entity.Schedule;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ScheduleDAOImpl implements ScheduleDAO {
     @Override
-    public boolean add(Schedule schedule) {
-        return false;
+    public boolean add(Schedule a) throws SQLException, ClassNotFoundException {
+        String sql = "Insert Into schedule Values(?,?,?,?,?)";
+        return CrudUtil.executeUpdate(sql,a.getSchedulID(),a.getSchedulName(),a.getEquipID(),a.getSchedulAddTime(),a.getSchedulDuration());
+
     }
 
     @Override
-    public boolean delete(String s) {
-        return false;
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        String sql = "Delete from schedule where schId;";
+        return CrudUtil.executeUpdate(sql,s);
     }
 
     @Override
-    public boolean update(Schedule schedule) {
-        return false;
+    public boolean update(Schedule a) throws SQLException, ClassNotFoundException {
+        String sql = "Update schedule set schName =?,equId =?,schAddTime = ?,schAddMon =? where schId =?";
+        return CrudUtil.executeUpdate(sql,a.getSchedulName(),a.getEquipID(),a.getSchedulAddTime(),a.getSchedulDuration(),a.getSchedulID());
+
     }
 
     @Override
-    public Schedule search(String s) {
+    public Schedule search(String s) throws SQLException, ClassNotFoundException {
+        String sql ="Select * from schedule where schId=?";
+        ResultSet rst = CrudUtil.executeQuery(sql, s);
+        if (rst.next()){
+            return new Schedule(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+                    rst.getString(5)
+            );
+        }
+
         return null;
     }
 
     @Override
-    public ArrayList<Schedule> getAll() {
-        return null;
+    public ArrayList<Schedule> getAll() throws SQLException, ClassNotFoundException {
+        String sql ="Select * from schedule";
+        ResultSet rst = CrudUtil.executeQuery(sql);
+        ArrayList<Schedule>list = new ArrayList<>();
+
+        while (rst.next()){
+            list.add(new Schedule(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+                    rst.getString(5)
+            ));
+        }
+        return list;
     }
 }
