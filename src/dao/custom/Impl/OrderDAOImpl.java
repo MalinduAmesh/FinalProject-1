5860910e4e824dc4e528a23757d8f984771acf65
Malinducj <ameshmalindu006@gmail.com>
2020-09-15 16:00:42 +0530

    second das

--- src/dao/custom/Impl/OrderDAOImpl.java
+++ src/dao/custom/Impl/OrderDAOImpl.java
@@ -1,33 +1,60 @@
 package dao.custom.Impl;
 
+import dao.CrudUtil;
 import dao.custom.OrdersDAO;
 import entity.Orders;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class OrderDAOImpl implements OrdersDAO {
     @Override
-    public boolean add(Orders orders) {
-        return false;
+    public boolean add(Orders a) throws SQLException, ClassNotFoundException {
+        String sql = "Insert Into orders Values(?,?,?)";
+        return CrudUtil.executeUpdate(sql,a.getOrdersID(),a.getOrdersDate(),a.getOrdersCustomerID());
     }
 
     @Override
-    public boolean delete(String s) {
-        return false;
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+         String sql ="Delete From orders where ordId = ?";
+         return CrudUtil.executeUpdate(sql,s);
     }
 
     @Override
-    public boolean update(Orders orders) {
-        return false;
+    public boolean update(Orders a) throws SQLException, ClassNotFoundException {
+        String sql = "Update orders set date =? ,cusId =? where ordId = ?";
+        return CrudUtil.executeUpdate(sql,a.getOrdersDate(),a.getOrdersCustomerID(),a.getOrdersID());
+
     }
 
     @Override
-    public Orders search(String s) {
+    public Orders search(String s) throws SQLException, ClassNotFoundException {
+        String sql = "Select * orders where ordId =?";
+        ResultSet rst = CrudUtil.executeQuery(sql, s);
+        if (rst.next()){
+            return new Orders(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3)
+            );
+        }
         return null;
     }
 
     @Override
-    public ArrayList<Orders> getAll() {
-        return null;
+    public ArrayList<Orders> getAll() throws SQLException, ClassNotFoundException {
+        String sql ="Select * from orders";
+        ResultSet rst = CrudUtil.executeQuery(sql);
+        ArrayList<Orders>list = new ArrayList<>();
+
+        while (rst.next()){
+            list.add(new Orders(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3)
+            ));
+        }
+        return list;
     }
 }
