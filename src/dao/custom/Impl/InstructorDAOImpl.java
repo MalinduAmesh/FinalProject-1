b0bddf2256c8dff5c37385ce4cbeab087882664b
Malinducj <ameshmalindu006@gmail.com>
2020-09-15 15:47:54 +0530

    second das

--- src/dao/custom/Impl/InstructorDAOImpl.java
+++ src/dao/custom/Impl/InstructorDAOImpl.java
@@ -46,7 +46,22 @@ public class InstructorDAOImpl implements InstructorDAO {
     }
 
     @Override
-    public ArrayList<Instructor> getAll() {
-        return null;
+    public ArrayList<Instructor> getAll() throws SQLException, ClassNotFoundException {
+        String sql = "Select * from instructor";
+        ResultSet rst = CrudUtil.executeQuery(sql);
+        ArrayList<Instructor>list = new ArrayList<>();
+        while (rst.next()){
+            list.add(new Instructor(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+                    rst.getString(5),
+                    rst.getString(6),
+                    rst.getString(7)
+            ));
+        }
+
+        return list;
     }
 }
