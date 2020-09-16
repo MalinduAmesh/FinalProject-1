d68ac461b5c10c2649307f335f588df6f8a9af10
carlos <ameshmalindu006@gmail.com>
2020-09-16 20:54:17 +0530

    Updates

---
+++ src/controller/MainFormController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.scene.control.Label;
+
+public class MainFormController {
+    public Label txtTotalEarnings;
+    public Label txtTotalGoldMembers;
+    public Label txtTotalInstructors;
+    public Label txtTotalSupliment;
+    public Label txtTotalEquipment;
+    public Label txtTotalGenarlMembers;
+}
